ec30390253f2bfd9848af9c8c4959ed78e995177
Ramana <ambatidurgavenkataramana5@gmail.com>
2026-01-20 11:49:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.post("/api/feedback", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const text = `
+-------------------------
+Name: ${name}
+Email: ${email}
+Message: ${message}
+Date: ${new Date().toLocaleString()}
+-------------------------
+
+`;
+
+  fs.appendFileSync("feedback.txt", text);
+  res.json({ status: "saved" });
+});
+
+
+app.get("/feedback", (req, res) => {
+  if (!fs.existsSync("feedback.txt")) {
+    return res.send("No feedback available yet.");
+  }
+  res.sendFile(__dirname + "/feedback.txt");
+});
+
+app.listen(3000, () =>
+  console.log("Server running on http://localhost:3000")
+);
